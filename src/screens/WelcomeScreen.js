b3940e84cb9676f3e31ff089cd0729ac8874be2d
NirajModi17 <nirajmodi1701@gmail.com>
2024-04-16 18:42:39 +0530

    Commit

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,8 +1,10 @@
 import { View, Text, SafeAreaView, Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp , heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import { useNavigation } from '@react-navigation/native'
 
 export default function WelcomeScreen() {
+    const navigation = useNavigation();
   return (
     <SafeAreaView className="flex-1 flex justify-around bg-white">
         <View className='space-y-2 mt-10'>
@@ -16,7 +18,7 @@ export default function WelcomeScreen() {
         <View className='flex-row justify-center'>
             <Image source={require('../../assets/bot.png')} style={{height:hp(26),width:wp(77)}}/>
         </View>
-        <TouchableOpacity className='bg-slate-600 mx-5 p-4 rounded-2xl mb-10'>
+        <TouchableOpacity onPress={()=>navigation.navigate('Home')} className='bg-slate-600 mx-5 p-4 rounded-2xl mb-10'>
             <Text style={{fontSize:wp(6)}} className="text-center font-bold text-white text-2xl">Get Started</Text>
         </TouchableOpacity>
     </SafeAreaView>
