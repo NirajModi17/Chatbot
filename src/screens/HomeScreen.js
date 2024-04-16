b420eaaf8f50325ca52c10f57922786e62135fa0
NirajModi17 <nirajmodi1701@gmail.com>
2024-04-16 18:55:44 +0530

    Commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
+import { widthPercentageToDP as wp , heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+        <View className="flex-row justify-center mt-2">
+          <Image source={require("../../assets/bot3.png")} style={{height:hp(25),width:hp(25)}} />
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
